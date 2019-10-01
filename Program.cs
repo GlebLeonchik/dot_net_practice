3cd105895f693958c5f572107889563dab0119d7
KozyrickiS <skozyricki@gmail.com>
2019-10-01 21:41:04 +0300

    Practic3

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Practic3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите элементы массива:");
+            Random rdn = new Random();
+            int[] mass = new int[6];
+            for(int i = 0; i < mass.Length; i++)
+            {
+                mass[i] = rdn.Next(1,101);
+            }
+            Console.WriteLine();
+            for(int i = 0; i < mass.Length; i++)
+            {
+                Console.WriteLine(mass[i]);
+            }
+            mass[2] *= 10;
+            Console.WriteLine();
+            for(int i = 0; i < mass.Length; i++)
+            {
+                Console.WriteLine(mass[i]);
+            }
+           
+            
+        }
+    }
+}
