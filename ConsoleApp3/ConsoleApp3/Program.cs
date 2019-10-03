853c599459e14daa35cec39eaa314a519003cffa
KozyrickiS <skozyricki@gmail.com>
2019-10-03 19:53:21 +0300

    Finished Practic

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic txt = ", guys!";
+            dynamic[] array = { 32, 'A', "Hello" };
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]+" ");
+            }
+            Console.WriteLine();
+            array[0] =array[0]+10;
+            array[2] = array[2] + txt;
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]+" ");
+            }
+        }
+       
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rdn = new Random();
+            int max=0;
+            int[] array= new int[13];
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = rdn.Next(1, 101);
+                Console.Write(array[i]+" ");
+            }
+            Console.WriteLine();
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (max < array[i])
+                    max = array[i];
+            }
+            Console.WriteLine("Максимальный элемент:"+max);
+        }
+    }
+}
