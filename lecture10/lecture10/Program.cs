a4643a5730be5538d1842ebef0cf5253f0466612
KozyrickiS <skozyricki@gmail.com>
2019-10-17 22:05:45 +0300

    Lecture10

---
+++ lecture10/lecture10/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+
+
+namespace lecture10
+{
+    interface ISquardR
+    {
+        void Squard(int radius);
+    }
+
+    class Round : ISquardR
+    {
+        double SquardR;
+        public void Squard(int radius)
+        {
+            SquardR=Math.PI*radius*radius;
+        }
+    }
+
+    interface ISquardP
+    {
+        void Squard(int a);
+    }
+
+    class Box : ISquardP
+    {
+        int SquardP;
+        public void Squard(int a)
+        {
+            SquardP = a * a; 
+        }
+    }
+
+    interface ISquard
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
