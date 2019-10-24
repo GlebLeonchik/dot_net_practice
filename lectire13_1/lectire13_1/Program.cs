94a24a00dddd5a41d7cbf50a9d4231d38fc44351
Gleb Leonchik <leowhite135@gmail.com>
2019-10-24 20:32:08 +0300

    Part 1

---
+++ lectire13_1/lectire13_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace lectire13_1
+{
+    class Song
+    {
+        string songName;
+        string autor;
+        string year;
+        string minutes;
+
+        public object GetSongData()
+        {
+            Song song = new Song();
+            Console.WriteLine("Song name?");
+            song.songName = Console.ReadLine();
+            Console.WriteLine("Autor?");
+            song.autor = Console.ReadLine();
+            Console.WriteLine("Song year?");
+            song.year = Console.ReadLine();
+            Console.WriteLine("Minutes?");
+            song.minutes = Console.ReadLine();
+            object info = new { song.autor, song.minutes, song.year };
+            return info;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song song = new Song();
+            object info = song.GetSongData();
+            Console.WriteLine(info);
+            Console.ReadKey();
+        }
+    }
+}
